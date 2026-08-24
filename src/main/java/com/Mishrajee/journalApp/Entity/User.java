f3903b1aa70a1f313ea4a161ee1f7bd105ad83f7
pravin602 <crinku974@gmail.com>
2026-08-25 00:29:46 +0530

    User Config

--- src/main/java/com/Mishrajee/journalApp/Entity/User.java
+++ src/main/java/com/Mishrajee/journalApp/Entity/User.java
@@ -12,7 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//user Config
 @Document(collection = "users")
 @Data
 @NoArgsConstructor
